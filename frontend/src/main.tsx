7e8b2fd40a2556f5bd24cc4d9a2d543c2972e178
Іван Каленич <ivankalenych@example.com>
2026-04-08 16:36:40 +0300

    Error commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { PostHogProvider } from "@posthog/react";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+  dsn: "https://eebe0858a148179a3c4f504ddccb7236@o4511184645914624.ingest.de.sentry.io/4511184655745104",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  environment: "production",
+  sendDefaultPii: true,
+});
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: "2026-01-30",
